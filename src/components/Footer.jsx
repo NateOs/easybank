1499ab59a05dc5ef42efa51482d9da0779cfd086
nateos <sodjanathan@gmail.com>
2021-07-30 13:13:55 +0000

    footer contents markup

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,61 @@
-import Button from "./Button"
+import logo from "../images/logo.svg";
+import fb from "../images/icon-facebook.svg";
+import youtube from "../images/icon-youtube.svg";
+import twitter from "../images/icon-twitter.svg";
+import pinterest from "../images/icon-pinterest.svg";
+import insta from "../images/icon-instagram.svg";
+import Button from "./Button";
+
 export default function Footer() {
-	return (
-		<div>
-			Footer
-			<Button/>
-		</div>
-	)
+  return (
+    <footer className="footer">
+      <div className="footer__media">
+        <img src={logo} alt="eazybank logo" />
+        {/* social media */}
+        <ul className="footer__socials">
+          <li>
+            <a href="eazyfb">
+              <img src={fb} alt="facebook" />
+            </a>
+          </li>
+          <li>
+            <a href="eazytwitter">
+              <img src={youtube} alt="twitter" />
+            </a>
+          </li>
+          <li>
+            <a href="eazyyoutube">
+              <img src={twitter} alt="youtube" />
+            </a>
+          </li>
+          <li>
+            <a href="eazypinterest">
+              <img src={pinterest} alt="pinterest" />
+            </a>
+          </li>
+          <li>
+            <a href="eazyinstagram">
+              <img src={insta} alt="insta" />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="footer__nav">
+        <a href="aboutus">About Us</a>
+        <a href="contact">Contact</a>
+        <a href="blog">Blog</a>
+      </div>
+      <div className="footer__nav">
+        <a href="careers">Careers</a>
+        <a href="support">Support</a>
+        <a href="privacy">Privacy Policy</a>
+      </div>
+      <div>
+        <div className="footer__cta">
+          <Button />
+        </div>
+        <p className="footer__copyright">© Easybank. All Rights Reserved</p>
+      </div>
+    </footer>
+  );
 }
