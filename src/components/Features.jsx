948b58e0decbe82e9345e267476e50ab66999750
nateos <sodjanathan@gmail.com>
2021-07-30 12:02:02 +0000

    minor changes

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,14 +2,12 @@ import FeatureItem from "./FeatureItem";
 export default function Features() {
   return (
     <div className="features">
-      <div className="features__text">
         <h1 className="text__title">Why choose Easybank?</h1>
         <p className="features__subtitle text__subtitle">
           {" "}
           We leverage Open Banking to turn your bank account into your financial
           hub. Control your finances like never before.
         </p>
-      </div>
       <FeatureItem />
     </div>
   );
