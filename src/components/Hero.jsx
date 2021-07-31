83a8a51a0e2a24772ddd0362b751a7f2951e16a4
nateos <sodjanathan@gmail.com>
2021-07-31 19:58:31 +0000

    major issues resolved z-index used, relative positioning + abs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,13 @@
 import Button from "./Button";
+import mockups from "../images/image-mockups.png"
 
 export default function Hero() {
   return (
     <div className="hero">
       {/* bg image */}
-      <div className="hero__image"></div>
+      <div className="hero__image">
+<img className="desktop-image" src={mockups} alt="" />
+      </div>
       <div className="hero__text">
         <h1 className="hero__title">Next generation digital banking</h1>
         <p className="hero__subtitle">
