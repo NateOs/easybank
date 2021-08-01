31d7b044e8a218550342b2ac7686c0165475fafa
nateos <sodjanathan@gmail.com>
2021-08-01 16:16:04 +0000

    fixed navbar on all vwports, fine tuning + bugfix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,32 +6,36 @@ import Button from "./Button";
 export default function Navbar() {
   return (
     <nav className="nav">
+
       {/* logo */}
       <a href="localhost:3000/" className="nav__logo">
         <img src={logo} alt="eazybank" />
       </a>
+
       {/* nav links */}
-      <ul className="nav__links">
-        <li className="nav__link-item">
+      <div className="nav__links">
+        <p className="nav__link-item">
           <a href="localhost:3000/">Home</a>
-        </li>
-        <li className="nav__link-item">
+        </p>
+        <p className="nav__link-item">
           <a href="localhost:3000/">About</a>
-        </li>
-        <li className="nav__link-item">
+        </p>
+        <p className="nav__link-item">
           <a href="localhost:3000/">Contact</a>
-        </li>
-        <li className="nav__link-item">
+        </p>
+        <p className="nav__link-item">
           <a href="localhost:3000/">Blog</a>
-        </li>
-        <li className="nav__link-item">
+        </p>
+        <p className="nav__link-item">
           <a href="localhost:3000/">Careers</a>
-        </li>
-        <li>
-          {/* <Button /> */}
-          <Button />
-        </li>
-      </ul>
+        </p>
+      </div>
+      
+      {/* button */}
+      <div className="nav__button">
+        <Button />
+      </div>
+
       {/* menu icon */}
       <img src={hamburger} alt="menu" className="nav__menu" />
     </nav>
